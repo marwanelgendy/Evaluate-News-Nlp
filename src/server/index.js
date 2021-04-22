d1a1904c922c8234ae5d1b8697626f60259c0e6b
MarwanElgendy <marwantarek852@gmail.com>
2021-04-22 23:06:23 +0200

    Edit based on review of udacity

--- src/server/index.js
+++ src/server/index.js
@@ -19,8 +19,7 @@ app.use(cors())
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 app.post('/test', async (req, res) => {
